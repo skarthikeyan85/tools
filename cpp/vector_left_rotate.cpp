a71800edf5886cf6d3c563435c3fe8c2b4962367
Karthikeyan Subramanian <skarthikeyan85@gmail.com>
2017-12-07 12:42:19 -0800

    ref code fsort a vector with custome cmp func

--- cpp/vector_left_rotate.cpp
+++ cpp/vector_left_rotate.cpp
@@ -31,6 +31,24 @@ vector<int> array_left_rotation(vector<int> a, int n, int k) {
     return b;
 }
 
+struct Player {
+    string name;
+    int score;
+};
+
+bool compare(Player p1, Player p2) {
+    if (p1.score > p2.score)
+        return true;
+    if (p1.score == p2.score)
+        return p1.name < p2.name;
+    return false;
+}
+
+vector<Player> comparator(vector<Player> players) {
+    sort(players.begin(), players.end(), compare);
+    return players;
+}
+
 int main(){
     int n;
     int k;
@@ -43,6 +61,22 @@ int main(){
     for(int i = 0; i < n;i++)
         cout << output[i] << " ";
     cout << endl;
+
+    cin >> n;
+    vector< Player > players;
+    string name;
+    int score;
+    
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player p1;
+        p1.name = name, p1.score = score;
+        players.push_back(p1);
+    }
+
+    vector<Player > answer = comparator(players);
+    for(int i = 0; i < answer.size(); i++) {
+        cout << answer[i].name << " " << answer[i].score << endl;
+    }
     return 0;
 }
-
